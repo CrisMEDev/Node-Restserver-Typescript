2f1361f6e72b2c41421f4dd59b4630d22f580fa8
Cristian Morales <cr15moresp@gmail.com>
2021-06-25 00:06:15 -0500

    Se crearon los endpoints para el usuario

---
+++ controllers/usuario.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from 'express';
+
+// Obtener usuarios
+export const getUsuarios = ( req: Request, res: Response ) => {
+
+    res.json({
+        msg: 'get - usuarios'
+    });
+
+}
+
+// Obtener un usuario
+export const getUsuario = ( req: Request, res: Response ) => {
+
+    const { id } = req.params;
+
+    res.json({
+        msg: 'get - usuario',
+        id
+    });
+
+}
+
+// Crear usuario
+export const postUsuario = ( req: Request, res: Response ) => {
+
+    const { body } = req;
+
+    res.json({
+        msg: 'post - usuario'
+    });
+
+}
+
+// Actualizar usuario
+export const putUsuario = ( req: Request, res: Response ) => {
+
+    const { id } = req.params;
+    const { body } = req;
+
+    res.json({
+        msg: 'put - usuarios'
+    });
+
+}
+
+// Borrar usuario
+export const deleteUsuario = ( req: Request, res: Response ) => {
+
+    const { id } = req.params;
+
+    res.json({
+        msg: 'delte - usuarios'
+    });
+
+}
+
+
+
--- models/server.ts
+++ models/server.ts
@@ -1,15 +1,31 @@
 import express, { Application } from 'express';
 
 
+import * as userRoutes from '../routes/usuario';    // Si hay más exportaciones en el archivo indicado
+                                                    // se pueden llamar con userRoutes.miExportacion
+
+
 class Server {
 
     private app: Application;   // Lo mismo que express.Application
     private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios'
+    }
 
     constructor(){
 
         this.app  = express();
         this.port = process.env.PORT || '8080';
+
+        // Definir las rutas
+        this.routes();
+
+    }
+
+    routes(){
+
+        this.app.use( this.apiPaths.usuarios, userRoutes.default );
 
     }
 
