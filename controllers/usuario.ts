a8449eaa64b408913f9d187cf0ca0df6cca36e47
Cristian Morales <cr15moresp@gmail.com>
2021-06-25 23:04:26 -0500

    Endpoints para crear y actualizar usuario creados

--- controllers/usuario.ts
+++ controllers/usuario.ts
@@ -5,50 +5,112 @@ import Usuario from '../models/usuario';
 // Obtener usuarios
 export const getUsuarios = async( req: Request, res: Response ) => {
 
-    const usuarios = await Usuario.findAll();
+    
+    try {
 
-    res.json({
-        usuarios
-    });
+        const usuarios = await Usuario.findAll();
+    
+        res.json({
+            usuarios
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }
 
 }
 
 // Obtener un usuario
 export const getUsuario = async( req: Request, res: Response ) => {
 
-    const { id } = req.params;
+    
+    try {
 
-    const usuario = await Usuario.findByPk( id );
-
-    if ( !usuario ){ res.status(400).json({ msg: `No existe un usuario con el id ${id}` }) }
-
-    res.json({
-        usuario
-    });
+        const { id } = req.params;
+    
+        const usuario = await Usuario.findByPk( id );
+    
+        if ( !usuario ){ res.status(400).json({ msg: `No existe un usuario con el id ${id}` }) }
+    
+        res.json({
+            usuario
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }
 
 }
 
 // Crear usuario
-export const postUsuario = ( req: Request, res: Response ) => {
+export const postUsuario = async( req: Request, res: Response ) => {
 
     const { body } = req;
+    
+    try {
 
-    res.json({
-        msg: 'post - usuario',
-        body
-    });
+        const existeEmail = await Usuario.findOne({
+            where: {
+                email: body.email
+            }
+        });
+
+        if ( existeEmail ){
+            return res.status(400).json({
+                msg: `Ya existe un usuario con el email ${body.email}`
+            });
+        }
+        
+        const usuario = Usuario.build( body );
+        await usuario.save();
+    
+        res.json({
+            usuario
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }
 
 }
 
 // Actualizar usuario
-export const putUsuario = ( req: Request, res: Response ) => {
+export const putUsuario = async( req: Request, res: Response ) => {
 
     const { id } = req.params;
     const { body } = req;
 
-    res.json({
-        msg: 'put - usuarios'
-    });
+    try {
+
+        const usuario = await Usuario.findByPk( id );
+
+        if ( !usuario ){
+            return res.status(404).json({
+                msg: `No existe un usuario con el id: ${id}`
+            });
+        }
+
+        await usuario.update( body );
+    
+        res.json({
+            usuario
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+    }
 
 }
 
